783c814c4ac842e4bf160c6187828cf79d12d2d9
sahilshingate01 <sahilshingate0@gmail.com>
2026-03-24 00:47:28 +0530

    navbar created

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Search } from "lucide-react"
+
+const Navbar = () => {
+  return (
+    <div className="h-20 flex items-center justify-between p-4">
+      <div>
+        <h1 className="font-bold text-3xl text-black  font-sans tracking-widest uppercase">CINELIST</h1>
+      </div>
+      <div className="h-8 w-8 bg-amber-300 border-2 flex items-center justify-center p-3">
+        <button className=""> <Search size={22} /> </button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
