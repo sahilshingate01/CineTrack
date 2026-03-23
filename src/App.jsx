1d7906239bbb6b4541cf7a7ec5c560754dfba74e
sahilshingate01 <sahilshingate0@gmail.com>
2026-03-24 01:12:43 +0530

    stuctural line added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+
 import Navbar from "./Components/Navbar"
+
 
 const App = () => {
   return (
-    <div>
+    <div className="bg-gray-200">
       <Navbar />
+      <div className="border-b-6 border-black"></div>
+      
+      <div className="flex min-h-screen">
+
+        <div className="w-[70%]"></div>
+
+        <div className="w-[30%] border-l-4 border-black"></div>
+
+      </div>
     </div>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,6 +9,7 @@ const Navbar = () => {
       <div className="h-8 w-8 bg-amber-300 border-2 flex items-center justify-center p-3">
         <button className=""> <Search size={22} /> </button>
       </div>
+      
     </div>
   )
 }
